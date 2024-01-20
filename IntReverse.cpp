d38b7a71afa2d927296c4abbf6a4d1ce69785eae
Vladislav Sychugin <4myebay@mail.ru>
2024-01-20 17:27:27 +0500

    finish

--- IntReverse.cpp
+++ IntReverse.cpp
@@ -10,13 +10,19 @@ void reverse(int* p_arr)
 	}
 }
 
+void print_arr(int arr[])
+{
+	for (int i = 0; i < 10; i++)
+		std::cout << arr[i] << ' ';
+}
+
 int main()
 {
 	setlocale(LC_ALL, "RU_ru");
 	int arr[] = { 0, 9, 8, 7, 6, 1, 2, 3, 4, 5 };
+	std::cout << "Исходный массив:\n";
+	print_arr(arr);
 	reverse(arr);
-	for (int i = 0; i < 10; i++)
-	{
-		std::cout << arr[i] << ' ';
-	}
+	std::cout << "\nРеверсивный массив:\n";
+	print_arr(arr);
 }
